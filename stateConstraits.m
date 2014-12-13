6edc8e4685ae03e147debbc8c4890132385c8380
Manuel Schaich <manuel.schaich@me.com>
2014-12-13 15:01:02 +0000

    Working solver for the parametrised input and disturbance constraints.

---
+++ stateConstraits.m
@@ -0,0 +1,137 @@
+
+% Computation with parametrised constraints. (Doesn't really work!)
+
+% V = [F*K,-ones(size(F,1),sizes.nAlpha),zeros(size(F,1),sizes.nBeta);
+%     G*KW,zeros(size(G,1),sizes.nAlpha),-ones(size(G,1),sizes.nBeta)];
+% v = ones(size(F,1)+size(G,1),1);
+% 
+% [V,v] = PreConditioning(V,v);
+% 
+% Vn =zeros(size(V));
+% vn = zeros(size(v));
+% 
+% for i = 1:size(V,1)
+%     [x,fval,exitflag,output,lambda] = cplexlp(-V(i,1:sizes.nX)*D,G,ones(size(G,1),1));
+%     Vn(i,:) = [V(i,1:sizes.nX)*(A+B*K),V(i,sizes.nX+1),V(i,sizes.nX+sizes.nAlpha+1)+V(i,1:sizes.nX)*x];
+%     vn(i) = v(i)-V(i,1:sizes.nX)*x;
+% end
+% 
+% paraMax = 20;
+% 
+% positivity = [zeros(sizes.nX),-eye(sizes.nAlpha+sizes.nBeta);
+%               zeros(sizes.nX),eye(sizes.nAlpha+sizes.nBeta)/paraMax];
+% 
+% Vpost = [];
+% vpost = [];
+% 
+% Vpre = [V;positivity];
+% vpre = [v;ones(2*sizes.nX,1)];
+% 
+% % isContained([V;positivity],[v;ones(sizes.nX,1)],[V;Vn;positivity],[v;vn;ones(sizes.nX,1)])
+% 
+% iter = 1;
+% iterMax = 50;
+% while and(~isContained(Vpre,vpre,Vpost,vpost),iter<iterMax)
+%     
+%     if iter~=1
+%        [Vpre,vpre] = PreConditioning(Vpost,vpost);
+%         Vn = zeros(size(Vpre));
+%         vn = zeros(size(vpre));
+%     end
+%     
+%     for i = 1:size(Vpre,1)
+%     [x,fval,exitflag,output,lambda] = cplexlp(-Vpre(i,1:sizes.nX)*D,G,ones(size(G,1),1));
+%     Vn(i,:) = [Vpre(i,1:sizes.nX)*(A+B*K),Vpre(i,sizes.nX+1),Vpre(i,sizes.nX+sizes.nAlpha+1)+Vpre(i,1:sizes.nX)*x];
+%     vn(i) = vpre(i)-Vpre(i,1:sizes.nX)*x;
+%     end
+% 
+% [Vpost,vpost] = linReduce([Vpre;Vn;positivity],[vpre;vn;ones(2*sizes.nX,1)]);
+% fprintf('At iteration %2.0d the number of constraints was %3.0d.\n',iter,length(vpost))
+% 
+% % Poly = Polyhedron(Vpost,vpost);
+% % 
+% % if ~Poly.isBounded
+% %     0;
+% % end
+% iter = iter+1;
+% end
+% 
+% 
+% [Lam,lam] = stepBack(Vpost,vpost,sizes);
+
+
+V = [F*K;G*KW];
+v = ones(size(V,1),1);
+
+Vn =zeros(size(V));
+vn = zeros(size(v));
+
+Vpost = [];
+vpost = [];
+
+Vpre = V;
+vpre = v;
+
+iter = 1;
+iterMax = 50;
+while and(~isContained(Vpre,vpre,Vpost,vpost),iter<iterMax)
+    
+        if iter~=1
+           [Vpre,vpre] = PreConditioning(Vpost,vpost);
+            Vn = zeros(size(Vpre));
+            vn = zeros(size(vpre));
+        end
+
+        for i = 1:size(Vpre,1)
+        [x,fval,exitflag,output,lambda] = cplexlp(-Vpre(i,:)*D,G,ones(size(G,1),1));
+        Vn(i,:) = Vpre(i,:)*(A+B*K);
+        vn(i) = vpre(i)+fval;
+        end
+    
+        if ~isempty(find(vn<0,1,'first'))
+            0;
+        end
+        
+    [Vpost,vpost] = linReduce([Vpre;Vn],[vpre;vn]);
+    fprintf('At iteration %2.0d the number of constraints was %3.0d.\n',iter,length(vpost))
+
+    iter = iter+1;
+end
+
+
+% Analysis plot
+
+% [x,y] = meshgrid(-3:0.5:3,-1.5:0.3:1.5);
+% UU = zeros(size(x));
+% VV = zeros(size(x));
+% dyns = real(logm(A+B*K));
+% for i = 1:size(x,1)
+%     for j = 1:size(x,2)
+%         T = dyns*[x(i,j);y(i,j)];
+%         UU(i,j) = T(1);
+%         VV(i,j) = T(2);
+%     end
+% end
+% 
+% figure(1)
+% plot(Polyhedron(Vpost,vpost))
+% hold on
+% quiver(x,y,UU,VV)
+% hold off
+
+nHor = 2;
+
+Lam = cell(1,nHor);
+lam = cell(1,nHor);
+
+Lam{1} = [Vpost,zeros(length(vpost),sizes.nAlpha+sizes.nBeta);
+          zeros(2*(sizes.nAlpha+sizes.nBeta),sizes.nX),[eye(sizes.nAlpha+sizes.nBeta);
+          -eye(sizes.nAlpha+sizes.nBeta)]];
+lam{1} = [vpost;20*ones(sizes.nAlpha+sizes.nBeta,1);ones(sizes.nAlpha+sizes.nBeta,1)];
+
+for  i = 2:nHor
+    [tLam,tlam] = stepBack(Lam{i-1},lam{i-1},sizes);
+    [tLam,tlam] = PreConditioning(tLam,tlam);
+    [Lam{i},lam{i}] = linReduce(tLam,tlam);
+end
+
---
+++ stepBack.m
@@ -0,0 +1,30 @@
+function [Aout,bout] = stepBack(Ain,bin,s)
+
+Vn = zeros(size(Ain,1),s.nX+s.nAlpha+s.nBeta+s.nU);
+vn = zeros(size(bin));
+
+for i = 1:size(Ain,1)
+    [~,fval,~,~,~] = cplexlp(-Ain(i,1:s.nX)*s.D,s.G,ones(size(s.G,1),1));
+    Vn(i,:) = [Ain(i,1:s.nX)*s.A,Ain(i,s.nX+1),Ain(i,s.nX+s.nAlpha+1)-fval,Ain(i,1:s.nX)*s.B];
+    vn(i) = bin(i)+fval;
+end
+
+V = [Vn;
+    zeros(size(s.F,1),s.nX),-ones(size(s.F,1),s.nAlpha),zeros(size(s.F,1),s.nBeta),s.F];
+v = [vn;ones(size(s.F,1),1)];
+
+% pP = fourier([Vn,vn],1:(s.nX+s.nAlpha+s.nBeta));
+% [pP,~] = esp([V,v],1:(s.nX+s.nAlpha+s.nBeta),0);
+% [Aout,bout] = linReduce(pP(:,1:end-1),pP(:,end));
+
+% P = Polyhedron(V,v);
+% 
+% if P.isBounded
+%     fprintf('The set is bounded.\n')
+% else
+%     fprintf('The set is unbounded.\n')
+% end
+% pP = P.projection(1:(s.nX+s.nAlpha+s.nBeta),'mplp');
+[Aout, bout] = myProjection(V,v,2);
+% Aout = pP.A;
+% bout = pP.b;
